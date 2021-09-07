609fce7f228625b29e8c990518b8cc50be9e2cd0
karavarichyan <narekkaravarichyan1910@mail.ru>
2021-09-07 23:31:07 +0400

    tnayin

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-print("hello")
+x = int(input('Enter a number: '))
+result = ''
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if x % 3 == 0:
+    result += 'Fizz'
+if x % 5 == 0:
+    result += 'Buzz'
+if result == '':
+    result = x
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(result)
