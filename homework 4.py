b7598caa2820246ada4b042fe916af40b30a7042
karavarichyan <narekkaravarichyan1910@mail.ru>
2021-09-13 23:21:16 +0400

    kisat tnayin

---
+++ homework 4.py
@@ -0,0 +1,26 @@
+# 1. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a nested loop.
+# Օգտագործելով ցիկլեր, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+for x in range(1, 10):
+    b = x ** 2
+    print(b)
+
+# 2. Create a 3x3 matrix that will contain the squares of the numbers from 1-9 using a list comprehension.
+# Օգտագործելով comprehension, ստեղծել 3x3 մատրից, որը կպարունակի 1-9 թվերի քառակուսիները։
+
+
+# 4. Count the number of 'b's in the given string. DO NOT use the count() method.
+# Հաշվել տրված սթրինգում 'b'-երի քանակը։ Չօգտագործել count() մեթոդը։
+nonsense = 'Blinking blindly, brainy Bob brings beautiful beer bottles beneath blue butterflies billowing brilliantly.'
+total = 0
+for a in nonsense:
+    if a == 'b' or a == 'B':
+        total += 1
+print(total)
+# 5. Write a program that will print the factorial of n.
+# Գրել ծրագիր, որը կտպի n թվի ֆակտորիալը։
+z = int(input("enter number"))
+total_1 = 1
+for c in range(1, z + 1):
+    total_1 *= c
+print(total_1)
--- porc.py
+++ porc.py
@@ -1,1 +1,11 @@
-print("Hello")
+x = 0
+y = 1
+z = 1
+for x in range(1, 10, 1):
+    for y in range(1, 10, 1):
+        z = x * y
+        if z < 10:
+            print(x, "*", y, "= ", z, end='')
+        else:
+            print(x, "*", y, "=", z, end='')
+    print()
